8fa897365be776cf8f686fb42c38a6d880200ed8
Yehor <egor.master.2015@gmail.com>
2026-05-01 00:01:44 +0200

    Added ContactForm and Footer

--- main/models.py
+++ main/models.py
@@ -10,3 +10,17 @@ class Test(models.Model):
     class Meta:
         verbose_name = 'Тест'
         verbose_name_plural = 'Тесты'
+
+class ContactModel(models.Model):
+    name = models.CharField(max_length=128, verbose_name="Ім'я")
+    email = models.EmailField(verbose_name="Електронна пошта")
+    phone = models.CharField(max_length=16, verbose_name="Номер телефону")
+    message = models.TextField(verbose_name="Текст повідомлення")
+    datetime = models.DateTimeField(verbose_name="Дата та час звернення", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email} - {self.phone} - {self.message}"
+
+    class Meta:
+        verbose_name = "Контактна форма"
+        verbose_name_plural = "Контактні форми"
