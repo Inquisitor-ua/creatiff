9b4f643d97bf6dccd5ee5ea9cd85e6626b1c183b
Yehor <egor.master.2015@gmail.com>
2026-05-23 17:45:28 +0200

    Fixed redirect after processing contact form

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import ContactForm
-from .models import ContactModel
+from .models import ContactModel, HomePage
 
 def process_contact_form(request):
     if request.method == 'POST':
@@ -10,7 +10,7 @@ def process_contact_form(request):
             #Дописати валідацію форми та збереження в БД
             form_data = form.cleaned_data
             ContactModel.objects.create(name = form_data['name'], email = form_data['email'], phone = form_data['phone'], message = form_data['message'])
-    return redirect(request.META.get('HTTP_REFERER', reverse('homepage')))
+    return redirect(request.META.get('HTTP_REFERER', HomePage.objects.live().first().url))
 
 def index(request):
     return render(request, 'main/index.html')
