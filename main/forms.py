3ef7ec3dc0350a8a9abd142059d0d45113f690d1
Yehor <egor.master.2015@gmail.com>
2026-06-02 13:40:41 +0200

    Moved the contact form to the snippet, added translation function

--- main/forms.py
+++ main/forms.py
@@ -1,33 +1,38 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(
-        label = "Su nombre",
-        widget = forms.TextInput(attrs={
+    name = forms.CharField()
+    email = forms.EmailField()
+    phone = forms.CharField(required=False)
+    message = forms.CharField(widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        self.settings = kwargs.pop('settings', None)
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].label = getattr(self.settings, 'name_label', '') or "You name"
+        self.fields['name'].widget = forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': "Escriban su nombre"
+            'placeholder': getattr(self.settings, 'name_placeholder', '') or "Input your name"
         })
-    )
-    email = forms.CharField(
-        label = "Su correo electrónico",
-        widget = forms.EmailInput(attrs={
+
+        self.fields['email'].label = getattr(self.settings, 'email_label', '') or "Your Email"
+        self.fields['email'].widget = forms.EmailInput(attrs={
             'class': 'form-control',
-            'placeholder': "email@example.com"
+            'placeholder': getattr(self.settings, 'email_placeholder', '') or "email@example.com"
         })
-    )
-    phone = forms.CharField(
-        label = 'Su teléfono',
-        required = False,
-        widget = forms.TextInput(attrs={
+
+
+        self.fields['phone'].label = getattr(self.settings, 'phone_label', '') or "Your phone"
+        self.fields['phone'].required = getattr(self.settings, 'phone_required', False)
+        self.fields['phone'].widget = forms.TextInput(attrs={
             'class': 'form-control',
-            'placeholder': '+34 951 392 250'
+            'placeholder': getattr(self.settings, 'phone_placeholder', '') or "+34 123 456 789"
         })
-    )
-    message = forms.CharField(
-        label = 'Su mensaje',
-        widget = forms.Textarea(attrs={
+
+        self.fields['message'].label = getattr(self.settings, 'message_label', '') or "Your message"
+        self.fields['message'].widget = forms.Textarea(attrs={
             'class': 'form-control',
-            'placeholder': 'Describa su consulta aquí...',
+            'placeholder': getattr(self.settings, 'message_placeholder', '') or "Describe your question here",
             'rows': 4,
         })
-    )
--- main/snippets.py
+++ main/snippets.py
@@ -27,7 +27,54 @@ class FooterSettings(models.Model):
     ]
 
     def __str__(self):
-        return self.caption
+        return self.caption or "Footer setting"
 
     class Meta:
         verbose_name = "Footer settings"
+
+@register_snippet
+class ContactFormSettings(TranslatableMixin, models.Model):
+    title = models.CharField(max_length=128, verbose_name="Form title", blank=True)
+    recipient_email = models.EmailField(blank=True, verbose_name="Email for receiving forms")
+
+    phone_required = models.BooleanField(default=False, verbose_name="Is phone required")
+
+    name_label = models.CharField(max_length=64, verbose_name="Label for field 'Name'", blank=True)
+    name_placeholder = models.CharField(max_length=128, verbose_name="Placeholder for field 'Name'", blank=True)
+    email_label = models.CharField(max_length=64, verbose_name="Label for field 'Email'", blank=True)
+    email_placeholder = models.CharField(max_length=128, verbose_name="Placeholder for field 'Email'", blank=True)
+    phone_label = models.CharField(max_length=64, verbose_name="Label for field 'Phone'", blank=True)
+    phone_placeholder = models.CharField(max_length=128, verbose_name="Placeholder for field 'Phone'", blank=True)
+    message_label = models.CharField(max_length=64, verbose_name="Label for field 'message'", blank=True)
+    message_placeholder = models.CharField(max_length=256, verbose_name="Placeholder for field 'Message'", blank=True)
+    submit_button_text = models.CharField(max_length=64, verbose_name="Confirmation button text", blank=True)
+    success_message = models.CharField(max_length=256, verbose_name="Message for сonfirmation that the form was successfully submitted", blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('recipient_email'),
+        FieldPanel('phone_required'),
+        FieldPanel('name_label'),
+        FieldPanel('name_placeholder'),
+        FieldPanel('email_label'),
+        FieldPanel('email_placeholder'),
+        FieldPanel('phone_label'),
+        FieldPanel('phone_placeholder'),
+        FieldPanel('message_label'),
+        FieldPanel('message_placeholder'),
+        FieldPanel('submit_button_text'),
+        FieldPanel('success_message'),
+    ]
+
+    def __str__(self):
+        return self.title or "Contact form settings"
+
+    class Meta:
+        verbose_name = "Contact form settings"
+        constraints = [
+            models.UniqueConstraint(
+                fields=('translation_key', 'locale'), 
+                name='unique_translation_key_locale_main_contactformsettings'
+            )
+        ]
+        
